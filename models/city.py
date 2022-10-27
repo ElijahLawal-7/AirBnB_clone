1a368162a9f74df7c795dd719b3faed250aad21c
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-10-27 01:53:55 -0700

    All about basemodel

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+from models.base_model import BaseModel
+"""Class inherence from BaseModel"""
+
+
+class City(BaseModel):
+    """Class for cities"""
+    state_id = ""
+    name = ""
