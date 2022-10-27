a5ec3cb5e67425488dafcb511c8950c1a8df4860
freedombridget <freedombridget0@gmail.com>
2022-10-27 00:38:05 -0700

    [Fix]: adding test for BaseModel instances

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Test suits for amenities
+"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """
+    Tests for amenities
+    """
+
+    c = City()
+
+    def test_class_exists(self):
+        """tests if class exists"""
+        self.assertEqual(str(type(self.c)), "<class 'models.city.City'>")
+
+    def test_user_inheritance(self):
+        """test if city is a subclass of BaseModel"""
+        self.assertTrue(self.c, City)
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.c, 'state_id'))
+        self.assertTrue(hasattr(self.c, 'name'))
+        self.assertTrue(hasattr(self.c, 'id'))
+        self.assertTrue(hasattr(self.c, 'created_at'))
+        self.assertTrue(hasattr(self.c, 'updated_at'))
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.c.state_id, str)
+        self.assertIsInstance(self.c.name, str)
+        self.assertIsInstance(self.c.id, str)
+        self.assertIsInstance(self.c.created_at, datetime.datetime)
+        self.assertIsInstance(self.c.updated_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()
