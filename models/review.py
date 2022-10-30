54ec1a7b70d12405e7d02fdb8a97978c0cfbb93b
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-10-30 15:24:58 -0700

    backend models

--- models/review.py
+++ models/review.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""The Review Class"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
-
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
-    """
+    """The Review Class"""
 
     place_id = ""
     user_id = ""
