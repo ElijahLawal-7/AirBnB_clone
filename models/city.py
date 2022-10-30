fb090d05c195ddae5b36d20e577bf52015f09149
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-10-30 08:10:34 -0700

    deletes

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/python3
-from models.base_model import BaseModel
-"""Class inherence from BaseModel"""
-
-
-class City(BaseModel):
-    """Class for cities"""
-    state_id = ""
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/python3
-from models.base_model import BaseModel
-"""Class inherence from BaseModel"""
-
-
-class State(BaseModel):
-    """Class for states"""
-    name = ""
