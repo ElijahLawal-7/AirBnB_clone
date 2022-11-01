647f92cf77192c1f7d413b191cddb6021ead0a41
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-11-01 05:35:17 -0700

    the console file

--- console.py
+++ console.py
@@ -11,6 +11,8 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
@@ -27,6 +29,7 @@ def parse(arg):
         retl = [i.strip(",") for i in lexer]
         retl.append(curly_braces.group())
         return retl
+
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter.
